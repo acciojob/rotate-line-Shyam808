5ec9ad7a1a44aafa7871ef4c1e43016614d52ab2
Shyam Paghadal <shyampaghadal@gmail.com>
2025-06-23 08:25:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+const line = document.getElementById("line");
+let angle = 0;
+
+setInterval(() => {
+  angle += 2;
+  line.style.transform = `rotate(${angle}deg)`;
+}, 20);
+
+// highlight bold words
+function highlight() {
+  const boldWords = document.querySelectorAll('strong');
+  boldWords.forEach(el => {
+    el.style.color = 'rgb(0, 128, 0)'; // Green
+  });
+}
+
+function return_normal() {
+  const boldWords = document.querySelectorAll('strong');
+  boldWords.forEach(el => {
+    el.style.color = 'rgb(0, 0, 0)'; // Black
+  });
+}
